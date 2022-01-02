b2f59798133d60ab6d176f793169a41503bc92b3
Digambar Tupurwadi <digz@kali>
2022-01-02 23:21:54 +0530

    Created Suggestions component

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../css/Home.css';
-import Post from './Post';
 import PostsWindow from './PostsWindow';
 import Stories from './Stories';
+import Suggestions from './Suggestions';
 
 const Home = () => {
     return (
@@ -11,85 +11,18 @@ const Home = () => {
 
                 <div className='left' >
                     <Stories />
+
+                    {/* PostsWindow :: consists of all the posts */}
                     <PostsWindow />
+
                 </div>
 
                 <div className='right' >
-                    <div className='head'>
-                        <div>
-                            <img src={require('../images/heart.png')} />
-                            <p style={{ paddingLeft: "23px" }} > <strong> <a href='#'>nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
-                        </div>
-                        <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Switch</a>
-                    </div>
-                    <div className='mid'>
-                        <div className='upper'>
-                            <span>Suggestions For You</span>
-                            <a href='#' style={{ fontWeight: 600, fontSize: "13px" }}>See All</a>
-                        </div>
-
-                        <div className='lower'>
-                            <div>
-
-                                <img src={require('../images/heart.png')} />
-                                <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
-                            </div>
-                            <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
-                        </div>
-                        <div className='lower'>
-                            <div>
-
-                                <img src={require('../images/heart.png')} />
-                                <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
-                            </div>
-                            <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
-                        </div>
-                        <div className='lower'>
-                            <div>
-
-                                <img src={require('../images/heart.png')} />
-                                <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
-                            </div>
-                            <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
-                        </div>
-                        <div className='lower'>
-                            <div>
-
-                                <img src={require('../images/heart.png')} />
-                                <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
-                            </div>
-                            <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
-                        </div>
-                        <div className='lower'>
-                            <div>
-
-                                <img src={require('../images/heart.png')} />
-                                <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
-                            </div>
-                            <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
-                        </div>
-
-                    </div>
-                    <div className='footer'>
-                        <div>
-                            <a href='#'>About</a>
-                            <a href='#'>Help</a>
-                            <a href='#'>Press</a>
-                            <a href='#'>API</a>
-                            <a href='#'>Jobs</a>
-                            <a href='#'>Privacy</a>
-                            <a href='#'>Terms</a><br />
-                            <a href='#'>Locations</a>
-                            <a href='#'>Top Accounts</a>
-                            <a href='#'>Hastags</a>
-                            <a href='#'>Language</a>
-                        </div>
-                        <div><a>© 2022 INSTAGRAM FROM META</a></div>
-                    </div>
+                    <Suggestions />
                 </div>
             </div>
         </div>
     )
 }
 
-export default Home
+export default Home;
---
+++ src/app/components/Suggestions.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const Suggestions = () => {
+    return (<>
+        <div className='head'>
+            <div>
+                <img src={require('../images/heart.png')} />
+                <p style={{ paddingLeft: "23px" }} > <strong> <a href='#'>nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
+            </div>
+            <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Switch</a>
+        </div>
+        <div className='mid'>
+            <div className='upper'>
+                <span>Suggestions For You</span>
+                <a href='#' style={{ fontWeight: 600, fontSize: "13px" }}>See All</a>
+            </div>
+
+            <div className='lower'>
+                <div>
+
+                    <img src={require('../images/heart.png')} />
+                    <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
+                </div>
+                <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
+            </div>
+            <div className='lower'>
+                <div>
+
+                    <img src={require('../images/heart.png')} />
+                    <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
+                </div>
+                <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
+            </div>
+            <div className='lower'>
+                <div>
+
+                    <img src={require('../images/heart.png')} />
+                    <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
+                </div>
+                <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
+            </div>
+            <div className='lower'>
+                <div>
+
+                    <img src={require('../images/heart.png')} />
+                    <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
+                </div>
+                <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
+            </div>
+            <div className='lower'>
+                <div>
+
+                    <img src={require('../images/heart.png')} />
+                    <p style={{ paddingLeft: "13px" }} > <strong> <a href='#' >nigga_sheth</a> </strong>  <br /> <span style={{ color: "#8e8e8e", fontSize: "13px" }}>Nigga Sheth</span></p>
+                </div>
+                <a href='#' style={{ color: "#0095f6", fontWeight: 600, fontSize: "13px" }}>Follow</a>
+            </div>
+
+        </div>
+        <div className='footer'>
+            <div>
+                <a href='#'>About</a>
+                <a href='#'>Help</a>
+                <a href='#'>Press</a>
+                <a href='#'>API</a>
+                <a href='#'>Jobs</a>
+                <a href='#'>Privacy</a>
+                <a href='#'>Terms</a><br />
+                <a href='#'>Locations</a>
+                <a href='#'>Top Accounts</a>
+                <a href='#'>Hastags</a>
+                <a href='#'>Language</a>
+            </div>
+            <div><a>© 2022 INSTAGRAM FROM META</a></div>
+        </div>
+    </>
+    )
+}
+
+export default Suggestions;
