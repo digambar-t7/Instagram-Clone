d5cf67cc715635c2e84a77758ee944ad29b82f5d
Digambar Tupurwadi <digz@kali>
2022-08-23 01:36:05 +0530

    enabled cors in spring-boot controller

--- backend/src/main/java/com/instagram/backend/controller/PostController.java
+++ backend/src/main/java/com/instagram/backend/controller/PostController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ public class PostController {
         this.postService = postService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("getallposts")
     public ResponseEntity<List<Post>> getAllPosts() {
         return ResponseEntity.ok(this.postService.getAllPosts());
