cdadfa0ddac8be50a3a3ed52ebbb7c1bbf96afcd
Digambar Tupurwadi <digz@kali>
2022-08-22 20:01:29 +0530

    created login page

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/Login.css';
 
 const Login = () => {
+
+    const [loginDetails, SetloginDetails] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleLoginDetails = (e) => {
+        SetloginDetails({ ...loginDetails, [e.target.name]: e.target.value });
+    }
+
     return (
         <div id='Login'>
-            this is login page
+            <div className="modal" >
+                <img className='inform' alt='instagram-logo' src={require("../images/instagram.png")} />
+                <input className='inform' type="text" name='username' value={loginDetails.username} onChange={handleLoginDetails} />
+                <input className='inform' type="password" name='password' value={loginDetails.password} onChange={handleLoginDetails} />
+                <button className='inform blue-btn'>Log in</button>
+                <Link className='frgtpwd inform' to={"/forgotpassword"}>Forgot password?</Link>
+            </div>
         </div>
     )
 }
